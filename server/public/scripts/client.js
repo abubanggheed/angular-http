fec45f7500cd3bd5d039f4f59b8fb7ab0a585e59
Elias Friedman <eliasfriedman@Eliass-MacBook-Pro.local>
2018-09-13 11:24:16 -0500

    $http get finishedand response prepared

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,13 @@
 const shellApp = angular.module('ShellApp', []);
 
-shellApp.controller('ShellController', [function () {
+shellApp.controller('ShellController', ['$http', function ($http) {
     let self = this;
     self.number = 7;
+
+    $http({
+        method: 'GET',
+        url: '/shells'
+    }).then(function(response){
+        console.log(response); 
+    });
 }]);
--- server/server.js
+++ server/server.js
@@ -4,8 +4,15 @@ const port = 5000;
 
 const app = express();
 
+let shells = [{ type: 'Conch', color: 'pink' },
+{ type: 'Blue', color: 'blue' }];
+
 app.use(express.static('server/public'));
 
 app.listen(port, () => {
     console.log('listening on port', port);
+});
+
+app.get('/shells', (req, res) => {
+    res.send(shells);
 });
